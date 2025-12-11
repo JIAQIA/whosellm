6f218bb55207d20a1bd1a08e161193538802aa0d
JQQ <jqq1716@gmail.com>
2025-12-11 14:12:17 +0800

    Bump version: 0.1.1a2 → 0.1.1a2.dev1

--- whosellm/__init__.py
+++ whosellm/__init__.py
@@ -7,7 +7,7 @@
 LLMeta - 统一的大语言模型版本和能力管理库 / A unified LLM model version and capability management library
 """
 
-__version__ = "0.1.1a2"
+__version__ = "0.1.1a2.dev1"
 
 from whosellm.capabilities import ModelCapabilities
 from whosellm.model_version import LLMeta
